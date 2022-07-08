9c7d2ce05135a01f060e0db97daf1f984b5f1862
Ivan Lanuza <icdlanuza@gmail.com>
2022-07-09 06:09:19 +0800

    Clean Template Base

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
+
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+  },
+});
