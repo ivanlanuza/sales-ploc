c7fba75b5837221743c391b8c3ed2740233cc1f0
Ivan Lanuza <icdlanuza@gmail.com>
2022-08-24 21:22:18 +0800

    Ploc Monthly Report

--- pages/insights/plocmonthly.js
+++ pages/insights/plocmonthly.js
@@ -51,7 +51,7 @@ export default function PlocMonthly({ users }) {
                   placeholder="All Users"
                   onChange={(e) => {
                     setFilterSelect(e.target.value);
-                  }}
+                  }} 
                 />
                 <StackedButtonPrimary
                   href=""
